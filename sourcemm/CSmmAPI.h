1f37ba9f7331997b33d86c226d0b81e9844e1e25
David Anderson <dvander@alliedmods.net>
2005-04-16 23:35:37 +0000

    Renamed file
    --HG--
    extra : convert_revision : svn%3Ac2935e3e-5518-0410-8daf-afa5dab7d4e3/trunk%4012

---
+++ sourcemm/CSmmAPI.h
@@ -0,0 +1,41 @@
+/* ======== SourceMM ========
+* Copyright (C) 2004-2005 Metamod:Source Development Team
+* No warranties of any kind
+*
+* License: zlib/libpng
+*
+* Author(s): David "BAILOPAN" Anderson
+* ============================
+*/
+
+#ifndef _INCLUDE_CSMM_API_H
+#define _INCLUDE_CSMM_API_H
+
+/**
+ * @brief Header for CSmmAPI implementation
+ * @file CSmmAPI.h
+ */
+
+#include "ISmmAPI.h"
+
+class CSmmAPI : public ISmmAPI
+{
+public:
+	ISmmPluginManager *PluginManager();
+	SourceHook::ISourceHook *SourceHook();
+	void LogMsg(ISmmPlugin *pl, const char *msg, ...);
+public:
+	CreateInterfaceFn engineFactory(bool syn=true);
+	CreateInterfaceFn physicsFactory(bool syn=true);
+	CreateInterfaceFn fileSystemFactory(bool syn=true);
+	CreateInterfaceFn serverFactory(bool syn=true);
+	CGlobalVars *pGlobals();
+	void SetLastMetaReturn(META_RES res);
+	META_RES GetLastMetaReturn();
+private:
+	META_RES m_Res;
+};
+
+extern CSmmAPI g_SmmAPI;
+
+#endif //_INCLUDE_CSMM_API_H
