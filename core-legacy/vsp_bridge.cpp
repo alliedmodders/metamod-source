9160ae786749f371f604e27cfa7d4d43043a701f
David Anderson <dvander@alliedmods.net>
2008-11-15 21:21:04 -0600

    Added VSP bridge source to core-legacy.

---
+++ core-legacy/vsp_bridge.cpp
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include "sourcemm.h"
+#include <loader_bridge.h>
+
+SH_DECL_HOOK0_void(ConCommand, Dispatch, SH_NOATTRIB, false);
+
+ConCommand *g_plugin_unload = NULL;
+bool g_bIsTryingToUnload;
+bool g_bIsBridgedAsVsp;
+
+void InterceptPluginUnloads()
+{
+	g_bIsTryingToUnload = true;
+}
+
+void InterceptPluginUnloads_Post()
+{
+	g_bIsTryingToUnload = false;
+}
+
+class VspBridge : public IVspBridge
+{
+	virtual bool Load(const vsp_bridge_info *info, char *error, size_t maxlength)
+	{
+		assert(!g_GameDll.loaded && !g_bIsBridgedAsVsp);
+
+		CreateInterfaceFn engineFactory = (CreateInterfaceFn)info->engineFactory;
+		CreateInterfaceFn gsFactory = (CreateInterfaceFn)info->gsFactory;
+
+		if (!AlternatelyLoadMetamod(engineFactory, gsFactory))
+			return false;
+
+		ConCommandBase *pBase = g_Engine.icvar->GetCommands();
+		while (pBase != NULL)
+		{
+			if (pBase->IsCommand() && strcmp(pBase->GetName(), "plugin_unload") == 0)
+			{
+				g_plugin_unload = (ConCommand *)pBase;
+				break;
+			}
+			pBase = const_cast<ConCommandBase *>(pBase->GetNext());
+		}
+
+		if (g_plugin_unload != NULL)
+		{
+			SH_ADD_HOOK_STATICFUNC(ConCommand, Dispatch, g_plugin_unload, InterceptPluginUnloads, false);
+			SH_ADD_HOOK_STATICFUNC(ConCommand, Dispatch, g_plugin_unload, InterceptPluginUnloads_Post, true);
+		}
+
+		extern ConVar metamod_version;
+		char buffer[255];
+
+		UTIL_Format(buffer, sizeof(buffer), "%sV", metamod_version.GetString());
+		metamod_version.SetValue(buffer);
+
+		g_bIsBridgedAsVsp = true;
+		g_pRealVspCallbacks = info->vsp_callbacks;
+
+		g_PluginMngr.SetVSPAsLoaded();
+
+		return true;
+	}
+
+	virtual void Unload()
+	{
+	}
+
+	virtual const char *GetDescription()
+	{
+		return "Metamod:Source " SOURCEMM_VERSION;
+	}
+};
+
+VspBridge mm14_vsp_bridge;
+
+SMM_API IVspBridge *
+GetVspBridge()
+{
+	return &mm14_vsp_bridge;
+}
