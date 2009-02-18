e8c1c45e719f2e353466af9d9097b7bfb23ee3c4
Scott Ehlert <ds@alliedmods.net>
2009-02-18 02:13:00 -0600

    PDB and DLL for Dark Messiah binary is now added to symbol server.
    DS, what are we going to do with you...

--- support/buildbot/symstore.pl
+++ support/buildbot/symstore.pl
@@ -24,6 +24,7 @@ symstore("loader\\msvc9\\server.*", $version);
 symstore("core-legacy\\msvc9\\Release\\metamod.1.ep1.*", $version);
 symstore("core\\msvc9\\Release - Orange Box\\metamod.2.ep2.*", $version);
 symstore("core\\msvc9\\Release - Left 4 Dead\\metamod.2.l4d.*", $version);
+symstore("core\\msvc9\\Release - Dark Messiah\\metamod.2.darkm.*", $version);
 
 #Lowercase DLLs.  Sigh.
 my (@files);
