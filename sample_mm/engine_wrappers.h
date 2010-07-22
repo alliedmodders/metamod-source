e84a28b429c6d8169f5c2cd81086b182df3debc2
Scott Ehlert <ds@alliedmods.net>
2010-07-22 03:22:28 -0500

    Added Alien Swarm build configs for sample plugins (bug 4529, r=dvander).

--- sample_mm/engine_wrappers.h
+++ sample_mm/engine_wrappers.h
@@ -70,13 +70,13 @@ public:
 
 inline int IndexOfEdict(const edict_t *pEdict)
 {
-	return (int)(pEdict - gpGlobals->baseEdict);
+	return (int)(pEdict - gpGlobals->pEdicts);
 }
 inline edict_t *PEntityOfEntIndex(int iEntIndex)
 {
 	if (iEntIndex >= 0 && iEntIndex < gpGlobals->maxEntities)
 	{
-		return (edict_t *)(gpGlobals->baseEdict + iEntIndex);
+		return (edict_t *)(gpGlobals->pEdicts + iEntIndex);
 	}
 	return NULL;
 }
