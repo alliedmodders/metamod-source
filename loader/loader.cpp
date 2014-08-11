alliedmodders/metamod-source

commit 013c549090445893a29a6c5634b96478dc4449ce

Improve automatic engine detection for some games (nobug, irc r=asherkin).
--HG--
extra : rebase_source : a80a7c0a00956617e58ce5ba196d37fb474061f2

loader/loader.cpp
@@ -270,7 +270,7 @@ mm_GetGameName(char *buffer, size_t size)
 }
 
 MetamodBackend
-mm_DetermineBackend(QueryValveInterface engineFactory, const char *game_name)
+mm_DetermineBackend(QueryValveInterface engineFactory, QueryValveInterface serverFactory, const char *game_name)
 {
 	if (engineFactory("VEngineServer024", NULL) != NULL)
 	{
@@ -287,14 +287,16 @@ mm_DetermineBackend(QueryValveInterface engineFactory, const char *game_name)
 	else if (engineFactory("VEngineServer022", NULL) != NULL &&
 		engineFactory("VEngineCvar007", NULL) != NULL)
 	{
-		if (strcmp(game_name, "berimbau") == 0)
+		if (serverFactory("ServerGameClients004", NULL))
 		{
+			if (strcmp(game_name, "portal2") == 0)
+			{
+				return MMBackend_Portal2;
+			}
+
 			return MMBackend_Blade;
 		}
-		if (strcmp(game_name, "portal2") == 0)
-		{
-			return MMBackend_Portal2;
-		}
+
 		if (engineFactory("EngineTraceServer004", NULL) != NULL)
 		{
 			return MMBackend_AlienSwarm;
@@ -310,6 +312,7 @@ mm_DetermineBackend(QueryValveInterface engineFactory, const char *game_name)
 				return MMBackend_Left4Dead2;
 			}
 		}
+
 		return MMBackend_Left4Dead;
 	}
 	else if (engineFactory("VEngineServer021", NULL) != NULL)

loader/gamedll.cpp
@@ -213,7 +213,7 @@ public:
 						 QueryValveInterface fileSystemFactory, 
 						 void *pGlobals)
 	{
-		mm_backend = mm_DetermineBackend(engineFactory, game_name);
+		mm_backend = mm_DetermineBackend(engineFactory, gamedll_qvi, game_name);
 
 		char error[255];
 		if (mm_backend == MMBackend_UNKNOWN)

loader/loader.h
@@ -120,7 +120,7 @@ extern void
 mm_GetGameName(char *buffer, size_t size);
 
 extern MetamodBackend
-mm_DetermineBackend(QueryValveInterface qvi, const char *game_name);
+mm_DetermineBackend(QueryValveInterface engineFactory, QueryValveInterface serverFactory, const char *game_name);
 
 extern MetamodBackend mm_backend;
 

loader/serverplugin.cpp
@@ -90,7 +90,7 @@ public:
 		{
 			mm_GetGameName(game_name, sizeof(game_name));
 
-			mm_backend = mm_DetermineBackend(engineFactory, game_name);
+			mm_backend = mm_DetermineBackend(engineFactory, gsFactory, game_name);
 		}
 
 		if (mm_backend == MMBackend_UNKNOWN)

commit ed6b5a49575e41988633f474c2632534727a9d7b

Fix broken Alien Swarm engine detection (regression from 013c5490904).

loader/loader.cpp
@@ -288,19 +288,19 @@ mm_DetermineBackend(QueryValveInterface engineFactory, QueryValveInterface serve
 	else if (engineFactory("VEngineServer022", NULL) != NULL &&
 		engineFactory("VEngineCvar007", NULL) != NULL)
 	{
-		if (serverFactory("ServerGameClients004", NULL))
+		if (engineFactory("EngineTraceServer004", NULL) != NULL)
 		{
+			if (engineFactory("XboxSystemInterface001", NULL) != NULL)
+			{
+				return MMBackend_AlienSwarm;
+			}
+			
 			if (strcmp(game_name, "portal2") == 0)
 			{
 				return MMBackend_Portal2;
 			}
 
 			return MMBackend_Blade;
-		}
-
-		if (engineFactory("EngineTraceServer004", NULL) != NULL)
-		{
-			return MMBackend_AlienSwarm;
 		}
 		else if (engineFactory("VPrecacheSystem001", NULL) != NULL)
 		{