6847a59c9c460b908ef030b72f5e4a940c966435
Kenzzer <14257866+kenzzer@users.noreply.github.com>
2025-08-15 01:05:19 +0000

    Update s2 sample extension to khook

--- samples/s2_sample_mm/src/plugin.h
+++ samples/s2_sample_mm/src/plugin.h
@@ -17,13 +17,14 @@
 
 #include <ISmmPlugin.h>
 #include <igameevents.h>
-#include <sh_vector.h>
 #include "version_gen.h"
 
 
 class MMSPlugin : public ISmmPlugin, public IMetamodListener
 {
 public:
+	MMSPlugin();
+
 	bool Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late);
 	bool Unload(char *error, size_t maxlen);
 	void AllPluginsLoaded();
@@ -35,14 +36,14 @@ public: //hooks
 				 bool loadGame,
 				 bool background );
 	void OnLevelShutdown();
-	void Hook_GameFrame( bool simulating, bool bFirstTick, bool bLastTick );
-	void Hook_ClientActive( CPlayerSlot slot, bool bLoadGame, const char *pszName, uint64 xuid );
-	void Hook_ClientDisconnect( CPlayerSlot slot, ENetworkDisconnectionReason reason, const char *pszName, uint64 xuid, const char *pszNetworkID );
-	void Hook_ClientPutInServer( CPlayerSlot slot, char const *pszName, int type, uint64 xuid );
-	void Hook_ClientSettingsChanged( CPlayerSlot slot );
-	void Hook_OnClientConnected( CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, const char *pszAddress, bool bFakePlayer );
-	bool Hook_ClientConnect( CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, bool unk1, CBufferString *pRejectReason );
-	void Hook_ClientCommand( CPlayerSlot nSlot, const CCommand &_cmd );
+	KHook::Return<void> Hook_GameFrame(IServerGameDLL*, bool simulating, bool bFirstTick, bool bLastTick );
+	KHook::Return<void> Hook_ClientActive(IServerGameClients*, CPlayerSlot slot, bool bLoadGame, const char *pszName, uint64 xuid );
+	KHook::Return<void> Hook_ClientDisconnect(IServerGameClients*, CPlayerSlot slot, ENetworkDisconnectionReason reason, const char *pszName, uint64 xuid, const char *pszNetworkID );
+	KHook::Return<void> Hook_ClientPutInServer(IServerGameClients*, CPlayerSlot slot, char const *pszName, int type, uint64 xuid );
+	KHook::Return<void> Hook_ClientSettingsChanged(IServerGameClients*, CPlayerSlot slot );
+	KHook::Return<void> Hook_OnClientConnected(IServerGameClients*, CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, const char *pszAddress, bool bFakePlayer );
+	KHook::Return<bool> Hook_ClientConnect(IServerGameClients*, CPlayerSlot slot, const char *pszName, uint64 xuid, const char *pszNetworkID, bool unk1, CBufferString *pRejectReason );
+	KHook::Return<void> Hook_ClientCommand(IServerGameClients*, CPlayerSlot nSlot, const CCommand &_cmd );
 public:
 	const char *GetAuthor() { return PLUGIN_AUTHOR; }
 	const char *GetName() { return PLUGIN_DISPLAY_NAME; }
@@ -52,6 +53,15 @@ public:
 	const char *GetVersion() { return PLUGIN_FULL_VERSION; }
 	const char *GetDate() { return __DATE__; }
 	const char *GetLogTag() { return PLUGIN_LOGTAG; }
+protected:
+	KHook::Virtual<IServerGameDLL, void, bool, bool, bool> m_GameFrame;
+	KHook::Virtual<IServerGameClients, void, CPlayerSlot, bool, const char *, uint64> m_ClientActive;
+	KHook::Virtual<IServerGameClients, void, CPlayerSlot, ENetworkDisconnectionReason, const char *, uint64, const char *> m_ClientDisconnect;
+	KHook::Virtual<IServerGameClients, void, CPlayerSlot, char const *, int, uint64> m_ClientPutInServer;
+	KHook::Virtual<IServerGameClients, void, CPlayerSlot> m_ClientSettingsChanged;
+	KHook::Virtual<IServerGameClients, void, CPlayerSlot, const char*, uint64, const char *, const char *, bool> m_OnClientConnected;
+	KHook::Virtual<IServerGameClients, bool, CPlayerSlot, const char*, uint64, const char *, bool, CBufferString *> m_ClientConnect;
+	KHook::Virtual<IServerGameClients, void, CPlayerSlot, const CCommand &> m_ClientCommand;
 };
 
 extern MMSPlugin g_ThisPlugin;
